9a4aca56a32584a1cab2b3041b15084143f63fc6
nickhoo <nickhoo555@gmail.com>
2019-03-19 02:11:29 +0800

    logo url  固定

--- webpack.config.js
+++ webpack.config.js
@@ -61,10 +61,10 @@ module.exports = {
         test: /iconfont\.(woff|woff2|eot|ttf|otf|svg)$/,
         use: ['file-loader?name=[path][name].[ext]?hash=[hash:7]']
       },
-      {
-        test: /apple-chancery-webfont\.(woff|woff2|eot|ttf|otf|svg)$/,
-        use: ['file-loader?name=[path][name].[ext]']
-      },
+      // {
+      //   test: /apple-chancery-webfont\.(woff|woff2|eot|ttf|otf|svg)$/,
+      //   use: ['file-loader?name=[path][name].[ext]']
+      // },
       {
         test: /HYYouYuanTiW\.(woff|eot|ttf|otf|svg)$/,
         use: ['file-loader?name=[path][name].[ext]']
